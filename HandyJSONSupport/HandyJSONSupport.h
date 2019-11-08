fdc8b401223ab290e1c640bdd0d911311571ad29
zevwings <zev.wings@gmail.com>
2019-11-08 09:47:58 +0800

    Initial Commit

---
+++ HandyJSONSupport/HandyJSONSupport.h
@@ -0,0 +1,19 @@
+//
+//  HandyJSONSupport.h
+//  HandyJSONSupport
+//
+//  Created by zevwings on 2019/11/8.
+//  Copyright © 2019 zevwings. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HandyJSONSupport.
+FOUNDATION_EXPORT double HandyJSONSupportVersionNumber;
+
+//! Project version string for HandyJSONSupport.
+FOUNDATION_EXPORT const unsigned char HandyJSONSupportVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HandyJSONSupport/PublicHeader.h>
+
+
